a21709c594f4b1b3adfd1115d70433c94bfeed14
PatrickKorie <94999760+PatrickKorie@users.noreply.github.com>
2022-04-19 14:28:34 -0400

    pause menu

--- Assets/User Folders/Ronan/Scripts/GameManager.cs
+++ Assets/User Folders/Ronan/Scripts/GameManager.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public bool GoalUnlocked;
     public int PickUpCount;
     int TotalFoundPickUp;
-
+    public GameObject PauseCanvas;
+    bool IsGamePaused;
     private void Start()
     {
+        PauseCanvas.SetActive(false);
         GoalUnlocked = false;
         var temp = GameObject.FindGameObjectsWithTag("PickUp");
         foreach (GameObject i in temp)
@@ -21,6 +24,11 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseTogle(true);
+        }
+
         if (PickUpCount >= TotalFoundPickUp)
         {
             GoalUnlocked = true;
@@ -33,5 +41,28 @@ public class GameManager : MonoBehaviour
         PickUpCount = PickUpCount + i;
     }
 
-
+    public void PauseTogle(bool pauseState)
+    {
+        IsGamePaused = pauseState;
+        if(IsGamePaused)
+        {
+            // pause
+            Time.timeScale = 0;
+            PauseCanvas.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            PauseCanvas.SetActive(false);
+            //unpause
+        }
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Options()
+    {
+        // show options
+    }
 }
