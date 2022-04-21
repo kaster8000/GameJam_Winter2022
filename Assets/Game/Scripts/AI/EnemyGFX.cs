e4b77153f5347a9f8b04ee7a794737feb9581c4b
JustMey <ronandaniel01@gmail.com>
2022-04-21 12:43:58 -0400

    Updated level scenes

--- Assets/Game/Scripts/AI/EnemyGFX.cs
+++ Assets/Game/Scripts/AI/EnemyGFX.cs
@@ -16,11 +16,11 @@ public class EnemyGFX : MonoBehaviour
     {
         if(M_AIPath.desiredVelocity.x >= 0.01f)
         {
-            transform.localScale = new Vector3(1f, transform.localScale.y, transform.localScale.z);
+            transform.localScale = new Vector3(-1f, transform.localScale.y, transform.localScale.z);
         }
         else if(M_AIPath.desiredVelocity.x <= -0.01f)
         {
-            transform.localScale = new Vector3(-1f, transform.localScale.y, transform.localScale.z);
+            transform.localScale = new Vector3(1f, transform.localScale.y, transform.localScale.z);
         }
 
         
