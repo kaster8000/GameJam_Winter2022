d3c002e2f52d1635df172e9030b4e543b4e73d73
PatrickKorie <94999760+PatrickKorie@users.noreply.github.com>
2022-04-21 19:52:51 -0400

    enemy audio changes

--- Assets/Game/Scripts/Other/GoalChanger.cs
+++ Assets/Game/Scripts/Other/GoalChanger.cs
@@ -5,16 +5,19 @@ using UnityEngine.SceneManagement;
 public class GoalChanger : MonoBehaviour
 {
     GameManager M_GameManager;
+    MusicController M_MusicController;
     public string LoadSceneName;
     void Start()
     {
         M_GameManager = FindObjectOfType<GameManager>();
+        M_MusicController = M_GameManager.GlobalMusicController;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player") && M_GameManager.GoalUnlocked)
         {
+            M_MusicController.StopAllMusic();
             SceneManager.LoadScene(LoadSceneName);
         }
     }
