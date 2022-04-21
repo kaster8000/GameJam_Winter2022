de4ba8c1debd98d7296263099b23e5079b9f1dc8
PatrickKorie <94999760+PatrickKorie@users.noreply.github.com>
2022-04-21 12:41:17 -0400

    Fixed level selector and audio manager

--- Assets/User Folders/Patrick/LevelUiInfo.cs
+++ Assets/User Folders/Patrick/LevelUiInfo.cs
@@ -34,7 +34,7 @@ public class LevelUiInfo : MonoBehaviour
     void LoadSelectedLevel()
     {
         Debug.Log(SceneName);
-        SceneManager.LoadScene("LoaderScene");
+        SceneManager.LoadScene(SceneName);
 
 
     }
