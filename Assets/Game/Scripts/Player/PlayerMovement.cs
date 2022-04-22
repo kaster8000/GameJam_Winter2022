0dd4f0b04fb48f0e2e613c47885898b6616ddfcd
Tricksterrz <89322416+Tricksterrz@users.noreply.github.com>
2022-04-22 01:17:47 -0400

    Finished Animations for Player and Enemy

--- Assets/Game/Scripts/Player/PlayerMovement.cs
+++ Assets/Game/Scripts/Player/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
 
     public bool CanMove;
     public float MoveSpeed;
+    public bool facingRight = true;
+    public Animator myAnim;
     Rigidbody2D Rb;
 
 
@@ -26,27 +28,39 @@ public class PlayerMovement : MonoBehaviour
             float MoveInputX = Input.GetAxis("Horizontal");
             float MoveInputY = Input.GetAxis("Vertical");
 
+            if (Rb.velocity.x > 0 && !facingRight) Flip();
+            else if (Rb.velocity.x < 0 && facingRight) Flip();
 
             Vector3 PlayerMove = new Vector3(MoveInputX, MoveInputY, 0);
             Rb.velocity = new Vector3(PlayerMove.x, PlayerMove.y, 0) * MoveSpeed;
-            
+            myAnim.SetFloat("moveVelocity", Rb.velocity.magnitude);
+            //myAnim.SetFloat("moveVelocity", Mathf.Abs(MoveInputY));
+
         }
         else
         {
             Rb.velocity = new Vector3(0, 0, 0);
-            
+            myAnim.SetFloat("moveVelocity", 0);
+
         }
-
-
-
-
-
-
     }
 
     public void TogleCanMove(bool i)
     {
         CanMove = i;
     }
+    void Flip()
+    {
+        facingRight = !facingRight;
 
+        if (facingRight == false)
+        {
+            gameObject.transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (facingRight == true)
+        {
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
 }
